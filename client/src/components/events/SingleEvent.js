5a52c55991fec13d44059982f7c24b11458391e0
ahmadabdolsaheb <ahmad.abdolsaheb@gmail.com>
2017-09-27 18:06:27 +0300

    bug in SingleEvent find function

--- client/src/components/events/SingleEvent.js
+++ client/src/components/events/SingleEvent.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
+function matched (event, id){
+  return event._id === id
+}
 
 class NewEventPage extends React.Component {
   render() {
-    console.log(this.props.match);
+    console.log(this.props.match.params.id);
+    console.log(this.props.event);
     return(
-      <div>
+      <div className="jumbotron">
+        <h1> poll </h1>
 
       </div>
     );
   }
 }
 
-export default NewEventPage;
+
+
+function mapStateToProps(state) {
+  return {
+    event: state.events.events.find(event => event._id === this.props.match.params.id)
+  }
+}
+
+export default connect(mapStateToProps, {}) (NewEventPage);
