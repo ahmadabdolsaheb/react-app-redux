55d4dd9b06f1c67d257a501b62c971f2ae987652
Ahmad Abdolsaheb <ahmad.abdolsaheb@gmail.com>
2017-09-28 19:19:31 +0300

    event info as states in component

--- client/src/components/events/SingleEvent.js
+++ client/src/components/events/SingleEvent.js
@@ -4,17 +4,51 @@ import{ loadAllEvents } from '../../actions/eventActions';
 import PropTypes from 'prop-types';
 
 class SingleEvent extends React.Component {
-  render() {
-    console.log(this.props.match.params.id);
-    console.log(this.props.event);
-    if(!this.props.event){
-      this.props.loadAllEvents();
-      console.log("hello");
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: false,
+      title: false,
+      username: false,
+      options:[]
     }
+    if(!this.props.events){
+      this.props.loadAllEvents();
+      console.log("Loading events");
+    }
+  }
+
+  eventFinder(events, id){
+    return events.find(
+      function ( item ){
+        return item._id === id;
+      }
+    );
+  }
+  componentDidUpdate(){
+    if(!this.state.id && this.props.events){
+      let eventId = this.eventFinder(this.props.events, this.props.match.params.id);
+      this.setState({
+        id: eventId._id,
+        title: eventId.title,
+        username: eventId.username,
+        options: eventId.options
+      })
+    }
+  }
+
+  render() {
+    console.log("id: " + this.state.id);
+    console.log("op: " + this.state.options);
+    let text = "loading ...";
+
+    if(this.state.id){
+      text = this.state.id;
+    }
+
     return(
       <div className="jumbotron">
-        <h1> poll </h1>
-
+        <h3>{text}</h3>
       </div>
     );
   }
@@ -26,13 +60,7 @@ SingleEvent.propTypes = {
 }
 
 function mapStateToProps(state) {
-  if(state.events.events){
-    return {event: state.events.events[0]}
-  }
-
-  else {
-    return {event: state.events.events}
-  }
+    return {events: state.events.events}
 }
 
 export default connect(mapStateToProps, { loadAllEvents}) (SingleEvent);
